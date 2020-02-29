e38f80de4d71bf6d46a24524fc3d96c2bf2b1129
Nicholas Davies <nicholasjohndavies42@gmail.com>
2020-02-29 19:01:15 +0800

    random symbol selection, length choice,passwordgen

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ var passwordText = document.querySelector("#password");
 // variables
 var lowLetters = ("qwertyuiopasdfghjklzxcvbnm");
 var capLetters = ("QWERTYUIOPASDFGHJKLZXCVBNM");
-var symbols = (",./;'[]\=-<>?:'`+_*&^%$#@!~");
+var symbols = (",./;'[]|}{:?><=-+_)(*&^%$#@!~`");
 var numbers = ("1234567890");
 var passwordPossibleCharacters = ("");
+var length = 0;
+var securePassword = "empty";
 
 var confirmLowLetters = false;
 var confirmCapLetters = false;
@@ -16,6 +18,24 @@ var confirmSymbols = false;
 // generate password:
 
 // Random Number Generator + picks random symbol.
+function randomSymbol() {
+  return(passwordPossibleCharacters[Math.floor(Math.random() * Math.floor((passwordPossibleCharacters.length)))]);
+}
+
+function lengthChoice() {
+  // loop to ensure they enter a password of correct length.
+  while(true) {
+    length = parseInt(prompt("How long would you like your password? must be between 8 and 128 characters."));
+    if (typeof(length) !== "number") {
+      alert("Not a number");
+    } else if (length >= 8 && length <= 128) {
+      alert("success!!");
+      return;
+    } else {
+      alert("Please enter a number between 8 and 128.");
+    }
+  }
+}
 
 // function to add potential symbols/letters/numbers
 function passConfirm(x, confx, varx) {
@@ -28,12 +48,19 @@ function passConfirm(x, confx, varx) {
 
 //questions
 function generatePassword() {
-  //lengthChoice();
+  lengthChoice();
   passwordPossibleCharacters = ("");
   passConfirm("Lowercase Letters", confirmLowLetters, lowLetters);
   passConfirm("Capital Letters", confirmCapLetters, capLetters);
   passConfirm("Symbols", confirmSymbols, symbols);
   passConfirm("Numbers", confirmNumbers, numbers);
+  
+  passwordPossibleCharacters.split("")
+  securePassword = "";
+  for(i = 0; i < length; i++) {
+    securePassword = securePassword + randomSymbol();
+  }
+  return(securePassword);
 }
 
 // Write password to the #password input
